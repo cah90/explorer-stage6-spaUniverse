d59765d22fce1bed288df04f3b6cac6b62769f9d
Cássia <cascrisbern@gmail.com>
2023-03-22 12:38:49 +0200

    added a method to change the background page

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 export class Router {
 	routes = {}
 
+	
 	add(routeName, page) {
 		this.routes[routeName] = page
 	}
@@ -18,6 +19,10 @@ export class Router {
 	handle() {
 		const { pathname } = window.location
 		const route = this.routes[pathname] || this.routes[404]
+		// console.log(pathname)
+		// console.log(route)
+
+		this.highlightLink(pathname)
 
 		fetch(route)
 			.then((data) => {
@@ -25,8 +30,44 @@ export class Router {
 			})
 			.then((html) => {
 				//console.log(html)
-
 				document.querySelector("#app").innerHTML = html
 			})
 	}
+
+	highlightLink(pathname) {
+		const home = document.querySelector("#home")
+		const universe = document.querySelector("#universe")
+		const exploration = document.querySelector("#exploration")
+
+		const body = document.querySelector("#body")
+
+
+		if(home.pathname == pathname) {
+			home.classList.add("active")
+			universe.classList.remove("active")
+			exploration.classList.remove("active")
+
+			body.classList.add("home")
+			body.classList.remove("universe")
+			body.classList.remove("exploration")
+
+		} else if(universe.pathname == pathname) {
+			universe.classList.add("active")
+			home.classList.remove("active")
+			exploration.classList.remove("active")
+
+			body.classList.remove("home")
+			body.classList.remove("exploration")
+			body.classList.add("universe")
+		} else {
+			exploration.classList.add("active")
+			universe.classList.remove("active")
+			home.classList.remove("active")
+
+			body.classList.remove("home")
+			body.classList.remove("universe")
+			body.classList.add("exploration")
+		}
+	}
+
 }
