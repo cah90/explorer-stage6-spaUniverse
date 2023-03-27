fd542ca2eeaa9d3e870246f46f23acb011b4e254
Cássia <cascrisbern@gmail.com>
2023-03-27 10:33:51 +0300

    changes

--- src/changeBackground.js
+++ src/changeBackground.js
@@ -7,21 +7,8 @@ export function changeBackground(pathname) {
 
 	const navigationItems = [home, universe, exploration]
 
-	// see here
-	// https://stackoverflow.com/questions/3010840/loop-through-an-array-in-javascript
-
 	for (const item of navigationItems) {
 		item.classList.remove("active")
-
-		// we take advantage here of the fact that the id (#) of each navigation item
-		// is equal with the class that we activate or deactivate on body
-		// For example, if we click on link with id='home' we need to add class home (same string as the id)
-		// to body, and remov ethe others
-
-		//console.log('id is')
-		//console.log(item.id)
-
-		// first we remove all classes from the body
 		body.classList.remove(item.id)
 	}
 
@@ -30,5 +17,5 @@ export function changeBackground(pathname) {
 			item.classList.add("active")
 			body.classList.add(item.id)
 		}
-	} /// for
+	}
 }
